7c2424afc4b986b7d53f9168ad1ef4d1ea1d31d2
akinator001 <aakashgandhi1279@gmail.com>
2020-10-17 21:13:53 +0530

    [aakash] Added : ability to read from and write on console

--- main/java/com/cp/payroll/EmployeePayrollService.java
+++ main/java/com/cp/payroll/EmployeePayrollService.java
@@ -1,7 +1,41 @@
 package com.cp.payroll;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
+	
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public EmployeePayrollService() {
+	}
+
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+
+	private void readEmployeePayrollData(Scanner sc) {
+		System.out.print("Enter Employee ID: ");
+		int id = sc.nextInt();
+		
+		System.out.print("Enter Employee Name: ");
+		String name = sc.next();
+		
+		System.out.print("Enter Employee Salary: ");
+		double salary = sc.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
+
+	private void writeEmployeePayrollData() {
+		System.out.println("\n Writing Employee Payroll Data to Console\n" + employeePayrollList);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to employee payroll.");
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner sc = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(sc);
+		employeePayrollService.writeEmployeePayrollData();
 	}
 }
