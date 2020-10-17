65e9f1749d85d079d0dbf1e5e2cd9ab9bfb4d8aa
akinator001 <aakashgandhi1279@gmail.com>
2020-10-17 21:54:02 +0530

    [aakash] Added : employee payroll data store into file

--- test/java/com/cp/payroll/EmployeePayrollServiceTest.java
+++ test/java/com/cp/payroll/EmployeePayrollServiceTest.java
@@ -4,42 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-public class EmployeePayrollServiceTest {
-	 	private static String HOME = System.getProperty("user.home");
-	    private static String PLAY_WITH_NIO = "TempPlayGround";
-
+public class EmployeePayrollServiceTest {	    
 	    @Test
-	    public void givenPathWhenCheckedThenCOnfirm() throws IOException {
-	    	
-	    	Path homePath = Paths.get(HOME);
-	    	Assert.assertTrue(Files.exists(homePath));
-
-	    	Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-	    	if(Files.exists(playPath))	
-	    		Files.delete(playPath);
-	    	Assert.assertTrue(Files.notExists(playPath));
-
-	    	Files.createDirectory(playPath);
-	    	Assert.assertTrue(Files.exists(playPath));
-
-	    	IntStream.range(1, 10).forEach(c -> {
-	    		Path tempFile = Paths.get(playPath + "/temp" + c);
-	    		Assert.assertTrue(Files.notExists(tempFile));
-	    		try {
-	    			Files.createFile(tempFile);
-	    			}
-	    		catch(IOException e) {}
-	    		Assert.assertTrue(Files.exists(tempFile));
-	    	});
-
-	    	Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
-	    	Files.newDirectoryStream(playPath).forEach(System.out::println);
-	    	Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
-
-	    }
+		public void given3EmployeesWhenWrittenToFileShouldMatchNumberOfEmployeeEntries() {
+			EmployeePayrollData[] arrayOfEmployees = { 
+					new EmployeePayrollData(1, "Aakash Gandhi", 5000.0),
+					new EmployeePayrollData(2, "sumit pandey", 4800.0),
+					new EmployeePayrollData(3, "Dhanraj Bansal", 6500.0) };
+			EmployeePayrollService employeePayrollService; 
+			employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+			employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+			Assert.assertEquals(3, employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO));
+		}	    
 }
